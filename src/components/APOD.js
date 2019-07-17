f4a03e13b37795d11fa0092989906d9f736020da
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-17 17:56:48 -0500

    got all data to display, now need to break it up into componenets

---
+++ src/components/APOD.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react"; 
+import axios from "axios"; 
+
+
+// function NewPic() {
+//     //https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15
+//     const [pic, setPic] = useState([]); 
+
+
+
+
+
+
+
+
+
+
+
+// }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react"; 
+import axios from "axios"
+import { directive } from "@babel/types";
+
+function Data() {
+    const [data, setData] = useState([]); 
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15')
+        .then(response => {
+            console.log(`nasa:api: ${response.data}`)
+            setData(response.data); 
+        })
+        .catch(error => {
+            console.log('There was an error:', error); 
+        })}, []); 
+
+        return (
+            <div>
+                <div>{data.title}</div>
+                <img src={data.url} alt="nasa of the day"></img>
+                <div>{data.explanation}</div>
+                <div>{data.date}</div>
+                <div>{data.copyright}</div>
+            </div>
+        )
+
+}
+
+export default Data; 
