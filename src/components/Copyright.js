9ddb7a6748ce3453b9952e578277815ef49c7cc8
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-18 17:33:34 -0500

    styled copyright with styled components

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,12 +1,17 @@
 import React from "react"; 
-
+import styled from "styled-components"; 
 
 function Copyright(props) {
 
+    const StyleDiv = styled.div`
+    color: #ffffff; 
+    font-size: 1.2rem; 
+    margin-top: 2rem; 
+    `
     return (
-        <div>
+        <StyleDiv>
             {props.copyright}
-        </div>
+        </StyleDiv>
     )
 }
 
